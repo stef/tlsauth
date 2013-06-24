831f024d8a89f97e314212454bee8e69f40dfce1
stef <stefan.marsiske@gmail.com>
2013-06-24 15:20:31 +0200

    [fix] correct pkg url

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ def read(fname):
 
 setup(
     name = "tlsauth",
-    version = "0.3",
+    version = "0.3.1-5",
     author = "Stefan Marsiske",
     author_email = "s@ctrlc.hu",
     license = "BSD",
     keywords = "crypto authentication TLS certificate x509 CA",
     py_modules=['tlsauth'],
-    install_requires = ['M2Crypto', 'pyOpenSSL', 'pyasn1'],
+    install_requires = ['M2Crypto', 'pyOpenSSL', 'pyasn1', 'pyspkac'],
     scripts=['bin/cert2pkcs12.sh', 'bin/createca.sh', 'bin/gencert.sh', 'bin/servercert.sh','bin/signcert.sh'],
-    url = "http://packages.python.org/tlsauth",
+    url = "https://pypi.python.org/pypi/tlsauth",
     description = "Implements TLS Authentication - simple client certificate CA inclusive",
     long_description=read('README.org'),
     classifiers = ["Development Status :: 4 - Beta",
