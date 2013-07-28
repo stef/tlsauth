880e4a52a7a7a72021550c4dc507f172bc52cc01
stef <stefan.marsiske@gmail.com>
2013-07-28 17:27:19 +0200

    [mod] removed spurious scripts from setup, and install.sh

--- setup.py
+++ setup.py
@@ -6,14 +6,13 @@ def read(fname):
 
 setup(
     name = "tlsauth",
-    version = "0.4",
+    version = "0.4-1",
     author = "Stefan Marsiske",
     author_email = "s@ctrlc.hu",
     license = "BSD",
     keywords = "crypto authentication TLS certificate x509 CA",
     py_modules=['tlsauth'],
     install_requires = ['M2Crypto', 'pyOpenSSL', 'pyasn1', 'pyspkac'],
-    scripts=['bin/cert2pkcs12.sh', 'bin/createca.sh', 'bin/gencert.sh', 'bin/servercert.sh','bin/signcert.sh'],
     url = "https://pypi.python.org/pypi/tlsauth",
     description = "Implements TLS Authentication - simple client certificate CA inclusive",
     long_description=read('README.org'),
